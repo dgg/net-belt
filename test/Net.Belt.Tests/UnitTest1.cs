41378d5b9b8aa9001112459cc533a194b60c8b36
Daniel González García <danielgonnet@gmail.com>
2025-10-25 13:50:03 +0200

    feat: :tada: project skeleton

---
+++ test/Net.Belt.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿namespace Net.Belt.Tests;
+
+public class Tests
+{
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	[Test]
+	public void Test1()
+	{
+		Assert.Fail("boom");
+	}
+
+	[Test]
+	public void Test2()
+	{
+		var subject = new Class1(3);
+		string five = subject.MultiMethod(2);
+
+		Assert.That(five, Is.EqualTo("5"));
+	}
+}
