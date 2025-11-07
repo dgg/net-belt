56e4585b78977966f15c72aea690ef55d0c4f043
Daniel González García <danielgonnet@gmail.com>
2025-11-07 09:16:58 +0100

    feat: :sparkles: add comparer by key selection

--- src/Net.Belt/Comparisons/Cmp.cs
+++ src/Net.Belt/Comparisons/Cmp.cs
@@ -12,5 +12,16 @@ public class Cmp<T>
 	/// <param name="next">The comparison logic to use for comparing objects of type <typeparamref name="T"/>.</param>
 	/// <param name="sortDirection">The sort direction, either <see cref="Direction.Ascending"/> or <see cref="Direction.Descending"/>. Defaults to <see cref="Direction.Ascending"/>.</param>
 	/// <returns>A <see cref="ChainableComparer{T}"/> that uses the specified comparison logic and sort direction.</returns>
-	public static ChainableComparer<T> By(Comparison<T> next, Direction sortDirection = Direction.Ascending) => new ComparisonComparer<T>(next, sortDirection);
+	public static ChainableComparer<T> By(Comparison<T> next, Direction sortDirection = Direction.Ascending) =>
+		new ComparisonComparer<T>(next, sortDirection);
+
+	/// <summary>
+	/// Creates a <see cref="ChainableComparer{T}"/> using the specified key selector and sort direction.
+	/// </summary>
+	/// <param name="keySelector">A function to extract a key from an element of type <typeparamref name="T"/>.</param>
+	/// <param name="sortDirection">The sort direction, either <see cref="Direction.Ascending"/> or <see cref="Direction.Descending"/>. Defaults to <see cref="Direction.Ascending"/>.</param>
+	/// <typeparam name="TKey">The type of the key returned by the key selector.</typeparam>
+	/// <returns>A <see cref="ChainableComparer{T}"/> that uses the specified key selector and sort direction.</returns>
+	public static ChainableComparer<T> By<TKey>(Func<T, TKey> keySelector,
+		Direction sortDirection = Direction.Ascending) => new SelectorComparer<T, TKey>(keySelector, sortDirection);
 }
