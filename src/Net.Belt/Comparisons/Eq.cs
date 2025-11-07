b8d49e1d0748ba096b7260cbd218ef3caa43baeb
Daniel González García <danielgonnet@gmail.com>
2025-11-07 13:27:57 +0100

    feat: :sparkles: add equalizer based on property selector

--- src/Net.Belt/Comparisons/Eq.cs
+++ src/Net.Belt/Comparisons/Eq.cs
@@ -16,4 +16,17 @@ public static class Eq<T>
 	/// <returns>A chainable equality comparer that applies the specified equality and hash code logic.</returns>
 	public static ChainableEqualizer<T> By(Func<T, T, bool> equals, Func<T, int> hasher) =>
 		new DelegatedEqualizer<T>(equals, hasher);
+
+	/// <summary>
+	/// Creates a chainable equality comparer by selecting a key from objects of type <typeparamref name="T"/>
+	/// using the provided key selector.
+	/// </summary>
+	/// <param name="keySelector">A function that extracts a key of type <typeparamref name="TKey"/> from an object of type <typeparamref name="T"/>.
+	/// The equality and hashing logic will be derived from this key.</param>
+	/// <typeparam name="TKey">The type of the key used for equality and hash code computation.</typeparam>
+	/// <returns>A chainable equality comparer that applies equality and hash code logic based on the extracted key.</returns>
+	public static ChainableEqualizer<T> By<TKey>(Func<T, TKey> keySelector)
+	{
+		return new SelectorEqualizer<T, TKey>(keySelector);
+	}
 }
