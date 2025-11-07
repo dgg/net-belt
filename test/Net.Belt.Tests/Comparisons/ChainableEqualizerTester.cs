995dca3ce6c07a87299228220039dfa5fe0fb1ce
Daniel González García <danielgonnet@gmail.com>
2025-11-07 10:37:12 +0100

    test: :white_check_mark: test chainable equalizer

---
+++ test/Net.Belt.Tests/Comparisons/ChainableEqualizerTester.cs
@@ -0,0 +1,45 @@
+using Net.Belt.Comparisons;
+using Net.Belt.Tests.Comparisons.Support;
+
+namespace Net.Belt.Tests.Comparisons;
+
+[TestFixture]
+public class ChainableEqualizerTester
+{
+	private static readonly EqualitySubject _a = new ("A", 4, 7.60m);
+	private static readonly EqualitySubject _b = new ("B", 1, 3.00m);
+
+	[Test, Category("Exploratory")]
+	public void Explore()
+	{
+		PropertySEqualizer inheritor = new PropertySEqualizer();
+		Assert.That(inheritor, Is.InstanceOf<IEqualityComparer<EqualitySubject>>(), "is a proper comparer");
+
+		EqualitySubject one = new EqualitySubject("1", 1, 1m), two = new EqualitySubject("1", 2, 2m);
+		Assert.That(inheritor.Equals(one, two), Is.True, "x.S == y.S");
+		Assert.That(inheritor.GetHashCode(one), Is.EqualTo(one.S!.GetHashCode()));
+
+		ChainableEqualizer<EqualitySubject> chained = inheritor.Then(new PropertyIEqualizer());
+		Assert.That(one, Is.Not.EqualTo(two).Using(chained), "x.I != y.I");
+	}
+
+
+	[Test]
+	public void SChainedComparer_CompareS()
+	{
+		var subject = new PropertySEqualizer();
+		Assert.That(subject.Equals(_a, new EqualitySubject { S = "A" }), Is.True);
+		Assert.That(subject.Equals(_a, _b), Is.False);
+	}
+
+	[Test]
+	public void Inheritors_DoNotHaveToCareAboutNulls()
+	{
+		var notNull = new EqualitySubject { S = "A" };
+
+		var chainable = new PropertySEqualizer();
+		Assert.That(chainable.Equals(notNull, null), Is.False);
+		Assert.That(chainable.Equals(null, notNull), Is.False);
+		Assert.That(chainable.Equals(null, null), Is.True);
+	}
+}
