b7faa27581714e0395a9706d13b0b59193caf9fc
Daniel González García <danielgonnet@gmail.com>
2025-11-25 13:05:34 +0100

    feat: :sparkles: Add delegate-based visitor pattern

--- src/Net.Belt/Patterns/Visitor/IVisitable.cs
+++ src/Net.Belt/Patterns/Visitor/IVisitable.cs
@@ -1,14 +1,15 @@
 namespace Net.Belt.Patterns.Visitor;
 
 /// <summary>
-/// 
+/// Represents an element that can be "visited" by a <see cref="IVisitor{TBase}"/>.
 /// </summary>
-/// <typeparam name="TBase"></typeparam>
+/// <remarks>"Visit" in this context means to perform a (potentially different) operation on a <typeparamref name="TBase"/> subtype.</remarks>
+/// <typeparam name="TBase">The base type of the elements that can be visited.</typeparam>
 public interface IVisitable<out TBase>
 {
 	/// <summary>
-	/// 
+	/// Accepts a <paramref name="visitor"/> to process this instance.
 	/// </summary>
-	/// <param name="visitor"></param>
+	/// <param name="visitor">The visitor that will process this instance.</param>
 	void Accept(IVisitor<TBase> visitor);
 }
--- src/Net.Belt/Patterns/Visitor/IVisitor.cs
+++ src/Net.Belt/Patterns/Visitor/IVisitor.cs
@@ -1,15 +1,15 @@
 namespace Net.Belt.Patterns.Visitor;
 
 /// <summary>
-/// 
+/// Represents a visitor that can operate on elements implementing <see cref="IVisitable{TBase}"/>.
 /// </summary>
-/// <typeparam name="TBase"></typeparam>
+/// <typeparam name="TBase">The base type of the elements that this visitor can visit.</typeparam>
 public interface IVisitor<in TBase>
 {
 	/// <summary>
-	/// 
+	/// Visits a specific <paramref name="visitable"/> instance.
 	/// </summary>
-	/// <param name="visitable"></param>
-	/// <typeparam name="TSub"></typeparam>
-	void Visit<TSub>(TSub visitable) where TSub : TBase;
+	/// <param name="visitable">The instance to visit.</param>
+	/// <typeparam name="TSub">The concrete type of the visitable instance.</typeparam>
+	void Visit<TSub>(TSub visitable) where TSub : TBase, IVisitable<TBase>;
 }
