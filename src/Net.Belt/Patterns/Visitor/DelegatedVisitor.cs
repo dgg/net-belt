1f8596a3fe1f425e3d7f05130ff5ad33624b2ee0
Daniel González García <danielgonnet@gmail.com>
2025-11-26 16:31:06 +0100

    fix: use Action instead of custom delegate

--- src/Net.Belt/Patterns/Visitor/DelegatedVisitor.cs
+++ src/Net.Belt/Patterns/Visitor/DelegatedVisitor.cs
@@ -8,14 +8,7 @@ namespace Net.Belt.Patterns.Visitor;
 /// Must implement <see cref="IVisitable{TBase}"/>.</typeparam>
 public class DelegatedVisitor<TBase> : IVisitor<TBase> where TBase : IVisitable<TBase>
 {
-	/// <summary>
-	/// Represents a delegate for a visit method that can handle a specific subtype of <typeparamref name="TBase"/>.
-	/// </summary>
-	/// <param name="u">The visitable object of type <typeparamref name="TSub"/> to be visited.</param>
-	/// <typeparam name="TSub">The concrete subtype of that this delegate can visit.</typeparam>
-	public delegate void VisitDelegate<in TSub>(TSub u) where TSub : TBase;
-	
-	readonly Dictionary<RuntimeTypeHandle, VisitDelegate<TBase>> _visitors = new();
+	readonly Dictionary<RuntimeTypeHandle, Action<TBase>> _visitors = new();
 
 	/// <summary>
 	/// Registers a visit method for a specific subtype <typeparamref name="TSub"/>.
@@ -24,7 +17,7 @@ public class DelegatedVisitor<TBase> : IVisitor<TBase> where TBase : IVisitable<
 	/// <typeparam name="TSub">The concrete subtype of <typeparamref name="TBase"/> that this visitor will handle.
 	/// Must implement <see cref="IVisitable{TBase}"/>.</typeparam>
 	/// <returns>The current <see cref="DelegatedVisitor{TBase}"/> instance, allowing for fluent chaining.</returns>
-	public DelegatedVisitor<TBase> AddVisitor<TSub>(VisitDelegate<TSub> visitor) where TSub : TBase, IVisitable<TBase>
+	public DelegatedVisitor<TBase> AddVisitor<TSub>(Action<TSub> visitor) where TSub : TBase, IVisitable<TBase>
 	{
 		_visitors.Add(typeof(TSub).TypeHandle, _adaptConcreteVisitor);
 		return this;
@@ -40,7 +33,7 @@ public class DelegatedVisitor<TBase> : IVisitor<TBase> where TBase : IVisitable<
 	public void Visit<TSub>(TSub visitable) where TSub : TBase, IVisitable<TBase>
 	{
 		RuntimeTypeHandle handle = typeof (TSub).TypeHandle;
-		if (_visitors.TryGetValue(handle, out VisitDelegate<TBase>? visit))
+		if (_visitors.TryGetValue(handle, out Action<TBase>? visit))
 		{
 			visit(visitable);
 		}
